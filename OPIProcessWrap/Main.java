6e0bc2285348597424795762f58352c9a631d62d
Hüseyin Can Gündüz <hcangunduz@gmail.com>
2022-07-12 11:00:23 +0300

    Library is generated

---
+++ OPIProcessWrap/Main.java
@@ -0,0 +1,7 @@
+import com.gmail.maystruks08.opi_core.Terminal;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Terminal.class.toString() );
+    }
+}
