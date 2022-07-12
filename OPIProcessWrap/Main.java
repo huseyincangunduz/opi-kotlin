a7d8f5faf0972c15ae742f68934f5121240565ba
Hüseyin Can Gündüz <hcangunduz@gmail.com>
2022-07-12 13:02:03 +0300

    Some corrections

--- OPIProcessWrap/Main.java
+++ OPIProcessWrap/Main.java
@@ -1,7 +1,32 @@
 import com.gmail.maystruks08.opi_core.Terminal;
+import com.gmail.maystruks08.opi_core.connector.OPILogger;
 
 public class Main {
+    public static class Logger implements OPILogger {
+
+        public void log(String message) {
+            System.out.println(message);
+        }
+
+        public void logError(Exception exception, String message) {
+            System.out.println(message);
+            exception.printStackTrace();
+        }
+
+        public void removeOutdateLogFiles() {
+
+        }
+    }
     public static void main(String[] args) {
+        final Terminal terminal = (new Terminal.Builder()).ipAddress("192.168.1.190")
+                .inputPort(20002).outputPort(20007).logger(new Logger()).build();
+        try {
+            terminal.login();
+        }
+        catch (Exception ex) {
+            ex.printStackTrace();
+        }
         System.out.println(Terminal.class.toString() );
+
     }
 }
